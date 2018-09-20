8c3bf48a14aa1dd96df2152f3711e4aaf3939240
rakazirut <kaz.brah@gmail.com>
2018-09-20 16:55:10 -0400

    Update: Basic Functions
    +, -, *, / working with some data validation for the operation field since i havent figured out buttons yet.

--- test.py
+++ test.py
@@ -20,13 +20,45 @@ def about():
 def my_form_post():
     int1 = (request.form['num1'])
     int2 = (request.form['num2'])
-    total = add(int(int1), int(int2))
-    new_text = "The Total is " + str(total)
-    return render_template('home.html', new_text=new_text)
+    operation = (request.form['op'])
+    result = compute(operation, int(int1), int(int2))
+
+    if result != 'null':
+        new_text = "The Total is " + str(result)
+        return render_template('home.html', new_text=new_text)
+    else:
+        new_text = "There was an error. Please try again."
+        return render_template('home.html', new_text=new_text)
+
+
+def compute(x, y, z):
+    if x == '+':
+        return add(y, z)
+    elif x == '-':
+        return subtract(y, z)
+    elif x == '*':
+        return multiply(y, z)
+    elif x == '/':
+        return divide(y,z)
+    else:
+        return 'null'
 
 
 def add(x, y):  # add two integers
     return x + y
+
+
+def multiply(x, y):  # multiply two integers
+    return x * y
+
+
+def subtract(x, y):  # subtract two integers
+    return x - y
+
+
+def divide(x, y):  # integer divide two integers
+    return x/y
+
 
 
 if __name__ == '__main__':
