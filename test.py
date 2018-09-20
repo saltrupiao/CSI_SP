ca9485f4ee05463aa8e8ccfed5a3326740757ad5
connor <dalek124@gmail.com>
2018-09-19 20:49:36 -0400

    Created a more complete Flask application. Testing CSS and page transitions using Flask

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
+# Placeholder for the application
 app = Flask(__name__)
 
 
+# This tells our program the route to our server
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True) # so the page refreshes live and doesn't need to be restarted
