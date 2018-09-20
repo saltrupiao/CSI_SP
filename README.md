9e32bec545212c3801ed730c647e0ad073e25a4a
rakazirut <kaz.brah@gmail.com>
2018-09-20 15:03:50 -0400

    Clone Connor

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 Beginning of the CSI Sophomore project repository. Details to come
 
 Rob: Testing Git Setup
-Rob:  Branch Testing
+
+Ben: practice pushing changes
+
+Connor: Testing PyCharm git integration with different branches
+
+This should only go in the "Connor-testBranch" branch
---
+++ test.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+# Placeholder for the application
+app = Flask(__name__)
+
+
+# This tells our program the route to our server
+@app.route('/')
+def hello_world():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+# https://stackoverflow.com/questions/12277933/send-data-from-a-textbox-into-flask
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['txt']
+    new_text = "You entered the word " + text
+    return render_template('home.html', new_text = new_text)
+
+
+if __name__ == '__main__':
+    app.run(debug=True) # so the page refreshes live and doesn't need to be restarted
