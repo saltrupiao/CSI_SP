12eac56723dd140638b815a7726a3f7792474a5a
rakazirut <kaz.brah@gmail.com>
2018-09-20 16:03:36 -0400

    Changed input for addition functionality

--- test.py
+++ test.py
@@ -18,9 +18,15 @@ def about():
 # https://stackoverflow.com/questions/12277933/send-data-from-a-textbox-into-flask
 @app.route('/', methods=['POST'])
 def my_form_post():
-    text = request.form['txt']
-    new_text = "You entered the word " + text
-    return render_template('home.html', new_text = new_text)
+    int1 = (request.form['num1'])
+    int2 = (request.form['num2'])
+    total = add(int(int1), int(int2))
+    new_text = "The Total is " + str(total)
+    return render_template('home.html', new_text=new_text)
+
+
+def add(x, y):  # add two integers
+    return x + y
 
 
 if __name__ == '__main__':
